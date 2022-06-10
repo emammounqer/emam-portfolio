e3b8145f04d2b4edfdae304809fc942b06737333
emam al mounqer <emam.anagreh@gmail.com>
2022-06-10 16:51:16 +0300

    add parameter 'once' to useInView hook

--- src/hooks/useInView.tsx
+++ src/hooks/useInView.tsx
@@ -2,24 +2,28 @@ import React, { useEffect, useRef, useState } from "react";
 
 export function useInView<T extends HTMLElement>(
   opt?: IntersectionObserverInit,
+  once?: boolean,
 ) {
   const ref = useRef<T>(null!);
   const [isInView, setIsItInView] = useState(true);
   const [entry, setEntry] = useState<IntersectionObserverEntry>();
 
-  const callBack: IntersectionObserverCallback = (entry) => {
-    setIsItInView(entry[0].isIntersecting);
-    setEntry(entry[0]);
-  };
-  const observer = new IntersectionObserver(callBack, opt);
-
   useEffect(() => {
+    const callBack: IntersectionObserverCallback = (entry) => {
+      setIsItInView(entry[0].isIntersecting);
+      setEntry(entry[0]);
+      if (entry[0].isIntersecting && once) {
+        observer.disconnect();
+      }
+    };
+
+    const observer = new IntersectionObserver(callBack, opt);
     const elem = ref.current;
     observer.observe(elem);
     return () => {
       observer.unobserve(elem);
     };
-  });
+  }, [ref, once]);
 
   return { ref, isInView, entry };
 }
